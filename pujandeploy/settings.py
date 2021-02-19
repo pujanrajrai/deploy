dfd637319e68b0121b0d5c3826e9d5be575d66a3
pujanrajrai <pujanr0809@gmail.com>
2021-02-19 23:35:37 +0545

    2nd commit

--- pujandeploy/settings.py
+++ pujandeploy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3_i@zcssx=4$9z-o-#19mop(j=8(cu!8vi*2wh_wu&h#cfx=ha'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "test1"
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,8 @@ ROOT_URLCONF = 'pujandeploy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates']
+        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
